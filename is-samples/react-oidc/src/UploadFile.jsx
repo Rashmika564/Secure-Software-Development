0eb84932cb0b2c5be32547eeae9eb745829a866a
Rashmika Rajapaksha <rashmikarajapaksha228@gmail.com>
2022-11-17 12:02:06 +0530

    file upload validation

--- is-samples/react-oidc/src/UploadFile.jsx
+++ is-samples/react-oidc/src/UploadFile.jsx
@@ -16,26 +16,39 @@ class UploadFile extends Component {
   }
 
   async submitFileData(event){
+
+
+   
     var that = this;
     event.preventDefault();
     console.log("test test",that.props.token)
     const data = new FormData();
     const imagedata = event.target.uploadFile.files[0];
     data.append('file', imagedata);
+    console.log(data);
     console.log(imagedata);
-    const responseData = await axios.post(`http://localhost:5000/file/upload`, data, {
-      headers: {
-        'Content-Type':'multipart/form-data',
-        'x-access-token': that.props.token.id_token,
-        'user_name':'chathura'
-      }
-    });
-    console.log(responseData);
-    if(responseData.status == 200){
 
-      console.log("file Uploaded")
-      // swal("Good job!", "File Uploaded Successfully!", "success");
+
+    if(imagedata){
+      const responseData = await axios.post(`http://localhost:5000/file/upload`, data, {
+        headers: {
+          'Content-Type':'multipart/form-data',
+          'x-access-token': that.props.token.id_token,
+          'user_name':'chathura'
+        }
+      });
+      console.log(responseData);
+      if(responseData.status == 200){
+  
+        console.log("file Uploaded")
+        // swal("Good job!", "File Uploaded Successfully!", "success");
+      }
     }
+    else{
+      alert("Enter a File")
+    }
+
+    
 
   }
 
